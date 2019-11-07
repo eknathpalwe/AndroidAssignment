3fd660c7299b31fc4279c465d7a30b5b38955a31
eknathpalwe131 <eknathpalwe131@gmail.com>
2019-11-07 10:29:48 +0530

    Init commit

---
+++ app/src/main/java/com/androidassignment/view/FactsListActivity.kt
@@ -0,0 +1,13 @@
+package com.androidassignment.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.androidassignment.R
+
+class FactsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facts_list)
+    }
+}
